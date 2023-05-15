3e3722f37123a0081b0fe7c4fcb866b51c1c3c6f
trancongchien <trancongchien185@gmail.com>
2023-05-15 23:42:20 +0700

    thêm Navigate

--- reactjsLab/lab bai 17/Lab 17.1 (Starter)/Lab 17.1 (Starter)/src/App.js
+++ reactjsLab/lab bai 17/Lab 17.1 (Starter)/Lab 17.1 (Starter)/src/App.js
@@ -2,12 +2,13 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Quotes from "./components/quotes/quotess/Quotes";
 import NewQuote from "./components/quotes/quotess/NewQuote";
 import QuoteDetai from "./components/quotes/quotess/QuoteDetails";
-
+import { Navigate } from "react-router-dom";
 const router = createBrowserRouter([
   {
     path: "/quotes",
     element: <Quotes />,
   },
+  { path: "/", element: <Navigate to="/quotes" /> },
   {
     path: "/new-quotes",
     element: <NewQuote />,
