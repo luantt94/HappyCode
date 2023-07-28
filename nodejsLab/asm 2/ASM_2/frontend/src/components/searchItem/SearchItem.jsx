3b13133f16bdfef61e6b0ab147f8697ab0e47b8c
trancongchien <trancongchien185@gmail.com>
2023-07-28 14:47:08 +0700

    search

--- nodejsLab/asm 2/ASM_2/frontend/src/components/searchItem/SearchItem.jsx
+++ nodejsLab/asm 2/ASM_2/frontend/src/components/searchItem/SearchItem.jsx
@@ -1,47 +1,30 @@
-import './searchItem.css';
+import "./searchItem.css";
+import { useNavigate } from "react-router-dom";
 
-const SearchItem = ({
-  name,
-  distance,
-  tag,
-  type,
-  description,
-  free_cancel,
-  price,
-  rate,
-  rate_text,
-  img_url,
-}) => {
+const SearchItem = ({ hotel }) => {
+  const navigate = useNavigate();
   return (
     <div className="searchItem">
-      <img src={img_url} alt="" className="siImg" />
+      <img src={hotel.photos[0]} alt="" className="siImg" />
       <div className="siDesc">
-        <h1 className="siTitle">{name}</h1>
-        <span className="siDistance">{distance} from center</span>
-        <span className="siTaxiOp">{tag}</span>
-        <span className="siSubtitle">{description}</span>
-        <span className="siFeatures">{type}</span>
-        {/* If can cancel */}
-        {free_cancel ? (
-          <div>
-            <span className="siCancelOp">Free cancellation </span>
-            <span className="siCancelOpSubtitle">
-              You can cancel later, so lock in this great price today!
-            </span>
-          </div>
-        ) : (
-          <div></div>
-        )}
+        <h1 className="siTitle">{hotel.name}</h1>
+        <span className="siDistance">{hotel.distance} from center</span>
+        <span className="siSubtitle">{hotel.description}</span>
+        <span className="siFeatures">{hotel.type}</span>
       </div>
       <div className="siDetails">
         <div className="siRating">
-          <span>{rate_text}</span>
-          <button>{rate}</button>
+          <button>{hotel.rating}</button>
         </div>
         <div className="siDetailTexts">
-          <span className="siPrice">${price}</span>
+          <span className="siPrice">${hotel.cheapestPrice}</span>
           <span className="siTaxOp">Includes taxes and fees</span>
-          <button className="siCheckButton">See availability</button>
+          <button
+            className="siCheckButton"
+            onClick={() => navigate(`/hotels/${hotel._id}`)}
+          >
+            See availability
+          </button>
         </div>
       </div>
     </div>
