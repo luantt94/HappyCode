14c937411e7723b5ab45e87a2899e4956c4ea42f
trancongchien <trancongchien185@gmail.com>
2023-10-09 23:40:57 +0700

    asm 3

--- nodejsLab/asm 2/-NJS301_assignment2-booking/back_end/router/searchRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const searchRouter = express.Router();
-const searchController = require('../controller/searchController');
-
-
-searchRouter.get('/search', searchController.searchController);
-
-exports.searchRouter = searchRouter;
