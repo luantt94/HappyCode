7ca07f71bcd5f349efb5925d1c4b899715ad4174
trancongchien <trancongchien185@gmail.com>
2023-11-12 10:07:17 +0700

    token

--- nodejsLab/asm 3/asm_3/server/middleware/verifyToken.js
+++ nodejsLab/asm 3/asm_3/server/middleware/verifyToken.js
@@ -2,18 +2,26 @@ import jwt from "jsonwebtoken";
 import User from "../models/user.models.js";
 import { createError } from "./error.js";
 
-// verify token
+// Tạo hàm verify token
 export const verifyToken = (req, res, next) => {
   const authHeader = req.get("Authorization");
   const token = authHeader && authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+
+  // Thay thế process.env.ACCESS_TOKEN_SECRET bằng giá trị cụ thể
+  const secret = "Y38fH7gO9P";
+
+  jwt.verify(token, secret, (err, user) => {
     if (err) return next(createError(403, "Token is not valid!"));
+
     User.findById(user._id).then((res) => {
       req.user = res;
       next();
     });
   });
 };
+
+// Các hàm verify user, verify counselors và verify admin sẽ được giữ nguyên.
+// Đảm bảo rằng chỉ thay đổi ACCESS_TOKEN_SECRET trong hàm verifyToken.
 
 // verify user
 export const verifyUser = (req, res, next) => {
